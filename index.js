02a7b697de9f39e6a2c7052a6af160cb58efbdbd
Shivansh Dwivedi <dwishwid05@gmail.com>
2024-04-02 00:42:39 +0530

    updated on 01/04/2024

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const searchWeather =document.querySelector('.search-weather-container')
 const loadingScreen = document.querySelector('.loading-screen-container')
 const weatherInfo = document.querySelector('.weather-information-container')
 const city = document.querySelector('#city')
-const countryFlag = document.querySelector('country-flag')
+const countryFlag = document.querySelector('#country-flag')
 const weather = document.querySelector('#weather')
 const weatherImg = document.querySelector('#weather-image')
 const temperature = document.querySelector('#temperature')
 const windData = document.querySelector('#wind-data')
 const humidityData = document.querySelector('#humidity-data')
 const cloudsData = document.querySelector('#clouds-data')
+const grantAccess = document.querySelector('#grant-access')
+const searchButton = document.querySelector('#search-btn')
+const cityName = document.querySelector('#city-name')
 
 const API_KEY = "898b6975708087bec2a7dc4725cdd1e7"
 
@@ -72,12 +75,72 @@ async function fetchWeatherInfo(cordinates){
     loadingScreen.classList.add('active')
 
     try {
-        const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY} `)
+        const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
         const data =await result.json()
         loadingScreen.classList.remove('active')
         weatherInfo.classList.add('active')
+        renderWeatherInfo(data)
         
     } catch (error) {
+        loadingScreen.classList.remove('active')
+    }
+}
+
+
+function renderWeatherInfo(infoWeather){
+    city.innerText = infoWeather?.name
+    countryFlag.src =`https://flagcdn.com/16x12/${infoWeather?.sys?.country.toLowerCase()}.png`
+    weather.innerText=infoWeather?.weather?.[0]?.description
+    weatherImg.src = `https://openweathermap.org/img/wn/${infoWeather?.weather?.[0]?.icon}.png`
+    temperature.innerText=infoWeather?.main?.temp
+    windData.innerText = infoWeather?.wind?.speed
+    humidityData.innerText=infoWeather?.main?.humidity
+    cloudsData.innerText=infoWeather?.clouds?.all
+} 
+
+function getlocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }
+    else{
+        alert("No geolocation support available")
+    }
+}
+
+function showPosition(position){
+    const userCordinates = {
+        lat:position.coords.latitude,
+        lon:position.coords.longitude
+    }
+
+    sessionStorage.setItem('user-cordinates',JSON.stringify(userCordinates))
+    fetchWeatherInfo(userCordinates)
+}
+grantAccess.addEventListener('click' , getlocation)
+
+searchWeather.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    if(cityName.value ==="") return
+    fetchSearchWeatherInfo(cityName.value)
+})
+
+
+async function fetchSearchWeatherInfo(city){
+    loadingScreen.classList.add('active')
+    weatherInfo.classList.remove('active')
+    locationAccess.classList.remove('active')
+
+
+    try {
+        let response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+        let data =await response.json()
+        loadingScreen.classList.remove('active')
+        weatherInfo.classList.add('active')
+        renderWeatherInfo(data)
+        
+    } catch (error) {
+        console.log(error)
         
     }
+
 }
